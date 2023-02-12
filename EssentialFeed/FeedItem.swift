603a23c7735f256c10c577055157776649e2b33e
Dilshod Zopirov <dilshodzopirov@gmail.com>
2023-02-12 11:03:51 +0500

    create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by dilshodzopirov on 12/02/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
+ 
